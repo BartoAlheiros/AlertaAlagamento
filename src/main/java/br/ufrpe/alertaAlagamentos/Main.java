ccfd30588049e3d7734c1d0379c91ded8d56fafb
BartoAlheiros <alheirosb@gmail.com>
2019-05-28 12:22:37 -0200

    probabilidade incondicional

--- src/main/java/br/ufrpe/alertaAlagamentos/Main.java
+++ src/main/java/br/ufrpe/alertaAlagamentos/Main.java
@@ -3,6 +3,7 @@ package br.ufrpe.alertaAlagamentos;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.apache.poi.ss.usermodel.Row;
@@ -14,6 +15,7 @@ import br.ufrpe.alertaAlagamentos.beans.Localidade;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
+		ArrayList<Localidade> localidades = new ArrayList<>();
 		File myFile = new File("C:\\Users\\b4rt_\\Downloads\\Risco de Alagamentos(4).xlsx");
 		FileInputStream fis = new FileInputStream(myFile);
 
@@ -26,23 +28,78 @@ public class Main {
 		// Get iterator to all the rows in current sheet
 		Iterator<Row> rowIterator = mySheet.iterator();
 
-		// Traversing over each row of XLSX file
+		/* Itera sobre as linhas da planilha, 
+		 * salvando cada localidade no Array de localidades
+		 *  */
+		
+		Row row = null;
+		
 		while (rowIterator.hasNext()) {
 
-			Row row = rowIterator.next();
+			row = rowIterator.next();
 
-			if(row.getRowNum() != 0) {
+			if((row.getRowNum() > 0) && (row.getRowNum() <= 48)) {
 
-				Localidade local = new Localidade();
+				Localidade localidade = new Localidade();
 
-				local.setNome(row.getCell(0).getStringCellValue());
-				local.setRiscoAlagamento((int)row.getCell(1).getNumericCellValue());
+				localidade.nome = row.getCell(0).getStringCellValue();
+				localidade.precip = (int)row.getCell(2).getNumericCellValue();
+				localidade.nivelMareh = (int)row.getCell(3).getNumericCellValue();
+				localidade.riscoAlagamento = (int)row.getCell(1).getNumericCellValue();
+				// localidade.riscoEminente = (int)row.getCell(4).getNumericCellValue();
 				
-				
+				localidades.add(localidade);
 			}
 			System.out.println("");
 		}
+		
+		for (int i = 0; i < localidades.size(); i++) {
+			System.out.println(localidades.get(i).nome + ", " +
+					localidades.get(i).precip + ", " + localidades.get(i).nivelMareh + ", " + localidades.get(i).riscoAlagamento );
+		}
+		
+		// System.out.println("última linha: " + row);
+		
+		myWorkBook.close();
+		
+		/* Classificador Ingênuo de Bayes. */
+		calculaRisco(localidades);
 
+	}
+
+	/**
+	 * Analisa os dados obtidos das localidades e classifica o risco de alagamento.
+	 * 
+	 * @param localidades
+	 * 
+	 * @return Risco Eminente de alagamento calculado. 3 - Muito Alto. 2 - Alto. 1 - Baixo.
+	 * 
+	 *  */
+	private static int calculaRisco(ArrayList<Localidade> localidades) {
+		int riscoAlagamentoAlto = 0;
+		int riscoAlagamentoMedio = 0;
+		int riscoAlagamentoBaixo = 0;
+		
+		/* Probabilidade não condicional */
+		for (int j = 0; j < localidades.size(); j++) {
+			if (localidades.get(j).riscoAlagamento == 3) {
+				riscoAlagamentoAlto++;
+			} else if (localidades.get(j).riscoAlagamento == 2) {
+				riscoAlagamentoMedio++;
+			} else if (localidades.get(j).riscoAlagamento == 1) {
+				riscoAlagamentoBaixo++;
+			}
+		}	
+		
+		
+		System.out.println("Alto: " + riscoAlagamentoAlto + " Médio: " + riscoAlagamentoMedio + " Baixo: " + riscoAlagamentoBaixo);
+		
+		if(Integer.toString(localidades.get((localidades.size() - 1)).riscoAlagamento).equals("?")) {
+			System.out.println("Achei a interrogação.");
+		}
+		
+		return 0;
+		
 	}   
 }
 
--- src/main/java/br/ufrpe/alertaAlagamentos/beans/Localidade.java
+++ src/main/java/br/ufrpe/alertaAlagamentos/beans/Localidade.java
@@ -2,63 +2,22 @@ package br.ufrpe.alertaAlagamentos.beans;
 
 public class Localidade {
 	
-	private String nome;
-	private float precip;
-	private int mareh;
-	private int riscoAlag; // risco atribuído a cada localidade, varia de 1 a 3 (1 - Baixo, 2 - Médio, 3 - Alto).
-	private int riscoEminente; // risco calculado pelo Naive Bayes(inicia com '0'). e pode assumir os valores como os de riscoAlag.
+	public String nome;
+	public int precip;
+	public int nivelMareh;
+	// public int riscoAlagamento; // risco atribuído a cada localidade, varia de 1 a 3 (1 - Baixo, 2 - Médio, 3 - Alto).
+	public int riscoAlagamento; // risco calculado pelo Naive Bayes(inicia com '0'). e pode assumir os valores como os de riscoAlag.
 	
-	public Localidade(String nome, float precip, int mareh, int riscoAlag) {
+	public Localidade(String nome, int precip, int nivelMareh, int riscoAlagamento) {
 		this.nome = nome;
 		this.precip = precip;
-		this.mareh = mareh;
-		this.riscoAlag = riscoAlag;
-		this.riscoEminente = 0;
+		this.nivelMareh = nivelMareh;
+		// this.riscoAlagamento = riscoAlagamento;
+		this.riscoAlagamento = 0;
 	}
 
 	public Localidade() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public float getPrecip() {
-		return precip;
-	}
-
-	public void setPrecip(float precip) {
-		this.precip = precip;
-	}
-
-	public int getMareh() {
-		return mareh;
-	}
-
-	public void setMareh(int mareh) {
-		this.mareh = mareh;
-	}
-
-	public int getRiscoAlagamento() {
-		return riscoAlag;
-	}
-
-	public void setRiscoAlagamento(int risco) {
-		this.riscoAlag = risco;
-	}
-
-	public int getRiscoEminente() {
-		return riscoEminente;
-	}
-
-	public void setRiscoEminente(int riscoEminente) {
-		this.riscoEminente = riscoEminente;
-	}
-	
-	
 }
