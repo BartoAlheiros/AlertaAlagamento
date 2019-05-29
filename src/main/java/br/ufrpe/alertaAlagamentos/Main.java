ff91864d186ee8d7c34775a3f1bc0e3122c8c955
BartoAlheiros <alheirosb@gmail.com>
2019-05-29 12:46:27 -0200

    probabilidades: incodicionais

--- src/main/java/br/ufrpe/alertaAlagamentos/Main.java
+++ src/main/java/br/ufrpe/alertaAlagamentos/Main.java
@@ -16,7 +16,7 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		ArrayList<Localidade> localidades = new ArrayList<>();
-		File myFile = new File("C:\\Users\\b4rt_\\Downloads\\Risco de Alagamentos(4).xlsx");
+		File myFile = new File("C:\\Users\\b4rt_\\Downloads\\LinhaDoTiro.xlsx");
 		FileInputStream fis = new FileInputStream(myFile);
 
 		// Finds the workbook instance for XLSX file
@@ -31,39 +31,39 @@ public class Main {
 		/* Itera sobre as linhas da planilha, 
 		 * salvando cada localidade no Array de localidades
 		 *  */
-		
+
 		Row row = null;
+		int ultimaLinha = 0;
 		
+
 		while (rowIterator.hasNext()) {
 
 			row = rowIterator.next();
+			
+			ultimaLinha = row.getSheet().getLastRowNum();
 
-			if((row.getRowNum() > 0) && (row.getRowNum() <= 48)) {
-
+			if((row.getRowNum() > 0) && (row.getRowNum() <= ultimaLinha)) {
+		
 				Localidade localidade = new Localidade();
 
 				localidade.nome = row.getCell(0).getStringCellValue();
-				localidade.precip = (int)row.getCell(2).getNumericCellValue();
-				localidade.nivelMareh = (int)row.getCell(3).getNumericCellValue();
-				localidade.riscoAlagamento = (int)row.getCell(1).getNumericCellValue();
-				// localidade.riscoEminente = (int)row.getCell(4).getNumericCellValue();
-				
+				localidade.precip = (int)row.getCell(1).getNumericCellValue();
+				localidade.nivelMareh = (int)row.getCell(2).getNumericCellValue();
+				localidade.riscoAlagamento = (int)row.getCell(3).getNumericCellValue();
 				localidades.add(localidade);
 			}
-			System.out.println("");
+
 		}
-		
+
 		for (int i = 0; i < localidades.size(); i++) {
 			System.out.println(localidades.get(i).nome + ", " +
 					localidades.get(i).precip + ", " + localidades.get(i).nivelMareh + ", " + localidades.get(i).riscoAlagamento );
 		}
-		
-		// System.out.println("última linha: " + row);
-		
+
 		myWorkBook.close();
-		
+
 		/* Classificador Ingênuo de Bayes. */
-		calculaRisco(localidades);
+		calculaRisco(localidades, ultimaLinha-1);
 
 	}
 
@@ -75,13 +75,30 @@ public class Main {
 	 * @return Risco Eminente de alagamento calculado. 3 - Muito Alto. 2 - Alto. 1 - Baixo.
 	 * 
 	 *  */
-	private static int calculaRisco(ArrayList<Localidade> localidades) {
+	private static int calculaRisco(ArrayList<Localidade> localidades, int totalOcorrencias) {
 		int riscoAlagamentoAlto = 0;
 		int riscoAlagamentoMedio = 0;
 		int riscoAlagamentoBaixo = 0;
 		
-		/* Probabilidade não condicional */
-		for (int j = 0; j < localidades.size(); j++) {
+		double pRiscoAlagamentoAlto = 0;
+		double pRiscoAlagamentoMedio = 0;
+		double pRiscoAlagamentoBaixo = 0;
+		
+		double pRiscoAltoPluviometriaAlta = 0;
+		double pRiscoMedioPluviometriaAlta = 0;
+		double pRiscoBaixoPluviometriaAlta = 0;
+		
+		double pRiscoAltoPluviometriaMedia = 0;
+		double pRiscoMedioPluviometriaMedia = 0;
+		double pRiscoBaixoPluviometriaMedia = 0;
+
+		double pRiscoAltoPluviometriaBaixa = 0;
+		double pRiscoMedioPluviometriaBaixa = 0;
+		double pRiscoBaixoPluviometriaBaixa = 0;
+		
+		/* Probabilidade não condicional
+		 * Contando ocorrências. */
+		for (int j = 0; j < localidades.size() - 1; j++) {
 			if (localidades.get(j).riscoAlagamento == 3) {
 				riscoAlagamentoAlto++;
 			} else if (localidades.get(j).riscoAlagamento == 2) {
@@ -90,16 +107,26 @@ public class Main {
 				riscoAlagamentoBaixo++;
 			}
 		}	
-		
+
+		/* Probabilidade não condicional
+		 * Calculando. */
+		pRiscoAlagamentoAlto = (double)riscoAlagamentoAlto/totalOcorrencias;
+		pRiscoAlagamentoMedio = (double)riscoAlagamentoMedio/totalOcorrencias;
+		pRiscoAlagamentoBaixo = (double)riscoAlagamentoBaixo/totalOcorrencias;
 		
 		System.out.println("Alto: " + riscoAlagamentoAlto + " Médio: " + riscoAlagamentoMedio + " Baixo: " + riscoAlagamentoBaixo);
+		System.out.printf("pRiscoAlagamentoAlto %.2f %n", pRiscoAlagamentoAlto);
+		System.out.printf("pRiscoAlagamentoMedio %.2f %n", pRiscoAlagamentoMedio);
+		System.out.printf("pRiscoAlagamentoBaixo %.2f ", pRiscoAlagamentoBaixo);
 		
-		if(Integer.toString(localidades.get((localidades.size() - 1)).riscoAlagamento).equals("?")) {
-			System.out.println("Achei a interrogação.");
-		}
 		
+
+//		if(Integer.toString(localidades.get((localidades.size() - 1)).riscoAlagamento).equals("?")) {
+//			System.out.println("Achei a interrogação.");
+//		}
+
 		return 0;
-		
+
 	}   
 }
 
